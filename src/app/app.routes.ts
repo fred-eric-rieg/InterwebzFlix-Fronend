cc703791f38b4a5ea8a9bbb1baa6a87d1639c7cd
Frederic Rieg <fredericrieg@protonmail.com>
2024-01-08 10:48:40 +0300

    added landingpage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'landingpage', pathMatch: 'full' },
+    { path: '', component: LandingpageComponent },
+];
---
+++ src/app/components/landingpage/landingpage.component.ts
@@ -0,0 +1,60 @@
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-landingpage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './landingpage.component.html',
+  styleUrl: './landingpage.component.scss'
+})
+export class LandingpageComponent implements OnInit {
+
+  @ViewChild('emailInput') emailElement: ElementRef;
+
+
+  constructor(private router: Router, private renderer: Renderer2) {
+    this.emailElement = new ElementRef('');
+  }
+
+
+  ngOnInit() {
+  }
+
+
+  gotToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  /**
+   * Validates the email input and redirects to the register page if the email is valid.
+   */
+  redirectIfValid() {
+    if (!this.isValid()) {
+      this.emailElement.nativeElement.focus();
+      this.emailElement.nativeElement.select();
+      let value = this.emailElement.nativeElement.value;
+      this.emailElement.nativeElement.value = '';
+      this.renderer.setAttribute(this.emailElement?.nativeElement, 'placeholder', 'Please enter a valid email address');
+      setTimeout(() => {
+        this.emailElement.nativeElement.value = value;
+      }, 2000);
+    } else {
+      // Redirect to register page with email as query parameter
+      this.router.navigate(['/signup'], { queryParams: { email: this.emailElement.nativeElement.value } });
+    }
+  }
+
+
+  /**
+   * Simple email validation with regular expression.
+   * @returns true if the email is valid, false otherwise
+   */
+  isValid() {
+    if (this.emailElement.nativeElement.value.length === 0) return false;
+    // Regular expression for email validation
+    else if (!this.emailElement.nativeElement.value.match('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')) return false;
+    else return true;
+  }
+}
