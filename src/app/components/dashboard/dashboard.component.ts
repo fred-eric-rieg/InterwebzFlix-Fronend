cea36c9145a78e9dc56c14c69214b909c43bdea9
Frederic Rieg <fredericrieg@protonmail.com>
2024-01-09 14:57:29 +0300

    removed artefacts

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { ButtonPrimaryDirective } from '../../shared/directives/button-primary.directive';
 import { BillboardComponent } from './billboard/billboard.component';
 import { HeaderComponent } from './header/header.component';
@@ -13,15 +13,6 @@ import { ContainerComponent } from './container/container.component';
 })
 export class DashboardComponent {
 
-  @ViewChild('slideMenu') slideMenu: ElementRef;
-  isMenuOpen: boolean = false;
 
-  @ViewChild('scifi_1') scifi_1: ElementRef;
-  @ViewChild('scifi_title') scifi_title: ElementRef;
-
-  constructor(private renderer: Renderer2) {
-    this.slideMenu = new ElementRef('');
-    this.scifi_1 = new ElementRef('');
-    this.scifi_title = new ElementRef('');
-  }
+  constructor() { }
 }
