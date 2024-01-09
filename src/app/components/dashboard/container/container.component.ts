d8c0afc030987cd51bef08c7ed830d08055c5357
Frederic Rieg <fredericrieg@protonmail.com>
2024-01-09 15:10:27 +0300

    animation & swtiching between videos

--- src/app/components/dashboard/container/container.component.ts
+++ src/app/components/dashboard/container/container.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../../shared/services/data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './container.component.html',
   styleUrl: './container.component.scss'
 })
 export class ContainerComponent {
 
+  constructor(public dataService: DataService) { }
+
+
+  selectVideo(video: any) {
+    this.dataService.selectedVideo.next(video)
+  }
 }
