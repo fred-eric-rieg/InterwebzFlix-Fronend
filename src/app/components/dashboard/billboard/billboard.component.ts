54529d87d2b79b11e87927c05b5b4f70757c3784
Frederic Rieg <fredericrieg@protonmail.com>
2024-01-09 14:58:04 +0300

    automated billboard

--- src/app/components/dashboard/billboard/billboard.component.ts
+++ src/app/components/dashboard/billboard/billboard.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ButtonSecondaryDirective } from '../../../shared/directives/button-secondary.directive';
 import { ButtonPrimaryDirective } from '../../../shared/directives/button-primary.directive';
+import { DataService } from '../../../shared/services/data.service';
+import { CommonModule, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-billboard',
   standalone: true,
-  imports: [ButtonSecondaryDirective, ButtonPrimaryDirective],
+  imports: [ButtonSecondaryDirective, ButtonPrimaryDirective, DatePipe, CommonModule],
   templateUrl: './billboard.component.html',
   styleUrl: './billboard.component.scss'
 })
-export class BillboardComponent {
+export class BillboardComponent implements OnInit {
 
+  constructor(public dataService: DataService) { }
+
+
+  ngOnInit() {
+
+  }
 }
