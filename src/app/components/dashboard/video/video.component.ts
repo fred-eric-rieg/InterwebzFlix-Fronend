0413676f4d07f6388d4f9a0919ff15f8edf5ef6c
Frederic Rieg <fredericrieg@protonmail.com>
2024-01-10 10:13:34 +0300

    added video comp

---
+++ src/app/components/dashboard/video/video.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { DataService } from '../../../shared/services/data.service';
+import { CommonModule, Location } from '@angular/common';
+import { ButtonSecondaryDirective } from '../../../shared/directives/button-secondary.directive';
+
+@Component({
+  selector: 'app-video',
+  standalone: true,
+  imports: [CommonModule, ButtonSecondaryDirective],
+  templateUrl: './video.component.html',
+  styleUrl: './video.component.scss'
+})
+export class VideoComponent {
+
+
+
+  constructor(public dataService: DataService, private _location: Location) {
+    
+  }
+
+
+  goBack() {
+    this._location.back();
+  }
+
+}
